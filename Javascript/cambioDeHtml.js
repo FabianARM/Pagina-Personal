8e0eb98f4a9d4b9101058e5bf11dfb6489c8390b
FabianARM <fabian.rojasmasis@ucr.ac.cr>
2020-04-21 23:55:57 -0600

    Pagina

---
+++ Javascript/cambioDeHtml.js
@@ -0,0 +1,9 @@
+
+
+var button = document.getElementsByClassName("first-col")[0].firstChild;
+
+button.addEventListener("click", myScript())
+function cambiar()
+{
+    document.getElementsByClassName("texto")[0].innerHTML = "Hola funciono";
+}
